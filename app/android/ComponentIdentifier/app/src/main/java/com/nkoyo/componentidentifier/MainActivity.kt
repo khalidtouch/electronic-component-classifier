036a4d52111f88b8b7da4253783e63caabf9cd0a
khalidtouch <khalidhulabaloo9@gmail.com>
2023-04-07 09:14:08 +0100

    implemented the splash screen

--- app/android/ComponentIdentifier/app/src/main/java/com/nkoyo/componentidentifier/MainActivity.kt
+++ app/android/ComponentIdentifier/app/src/main/java/com/nkoyo/componentidentifier/MainActivity.kt
@@ -10,10 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.nkoyo.componentidentifier.ui.theme.ComponentIdentifierTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             ComponentIdentifierTheme {
