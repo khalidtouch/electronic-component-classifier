45b429c05846ed364ff0ec4531a8335939be1c96
khalidtouch <khalidhulabaloo9@gmail.com>
2023-04-08 11:12:35 +0100

    implemented the permissions handling dialog and added an abort button

--- app/android/ComponentIdentifier/app/src/main/java/com/nkoyo/componentidentifier/ui/components/Buttons.kt
+++ app/android/ComponentIdentifier/app/src/main/java/com/nkoyo/componentidentifier/ui/components/Buttons.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,14 +23,67 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.nkoyo.componentidentifier.R
 import com.nkoyo.componentidentifier.ui.theme.LocalBlack
 import com.nkoyo.componentidentifier.ui.theme.LocalGray
 import com.nkoyo.componentidentifier.ui.theme.LocalWhite
+
+@Composable
+fun SecondaryButton(
+    modifier: Modifier = Modifier,
+    label: String,
+    onClick: () -> Unit = {},
+){
+    OutlinedButton(
+        modifier = modifier.clip(CircleShape),
+        onClick = onClick,
+        shape = CircleShape,
+        border = BorderStroke(
+            width = 1.dp,
+            color = LocalBlack,
+        )
+    ){
+        Text(
+            text = label,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = LocalBlack,
+            modifier = modifier.padding(vertical = 8.dp, horizontal = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun TertiaryButton(
+    modifier: Modifier = Modifier,
+    label: String,
+    onClick: () -> Unit = {},
+){
+    OutlinedButton(
+        modifier = modifier.clip(CircleShape),
+        onClick = onClick,
+        shape = CircleShape,
+        border = BorderStroke(
+            width = 1.dp,
+            color = Color.Transparent,
+        )
+    ){
+        Text(
+            text = label,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = LocalBlack,
+            modifier = modifier.padding(vertical = 8.dp, horizontal = 16.dp)
+        )
+    }
+}
+
 
 @Composable
 fun ShowRecordButton(
@@ -137,6 +192,15 @@ private fun CameraFlipButtonPreview(){
 @Preview
 private fun ShowRecordButtonPreview(){
     ShowRecordButton(
+        onClick = {}
+    )
+}
+
+@Composable
+@Preview
+private fun SecondaryButtonPreview(){
+    SecondaryButton(
+        label = "Click me!",
         onClick = {}
     )
 }
