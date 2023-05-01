5680e1d64a729489cdfd879e74958fa595d305e8
Waltberry <waltofuzim@gmail.com>
2023-05-01 23:58:01 +0100

    Updates

--- cv_part/Google-Image-Scraper/main.py
+++ cv_part/Google-Image-Scraper/main.py
@@ -28,14 +28,18 @@ if __name__ == "__main__":
     image_path = os.path.normpath(os.path.join(os.getcwd(), 'photos'))
 
     #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
-    search_keys = list(set(["getty images- Capacitors","Filament bulbs"]))
+    search_keys = list(set(["getty images Light-emitting diode", " Unsplash images Light-emitting diode", \
+                            "Burst images Light-emitting diode", "Pexels images Light-emitting diode", \
+                            "Pixabay images Light-emitting diode", "Free Images Light-emitting diode", \
+                            "Kaboompics Images Light-emitting diode", "Stocksnap Images Light-emitting diode", \
+                            "Flickr Images Light-emitting diode", "LEDs", "Light-emitting diode"]))
 
     #Parameters
-    number_of_images = 300                # Desired number of images
+    number_of_images = 1000             # Desired number of images
     headless = True                     # True = No Chrome GUI
     min_resolution = (0, 0)             # Minimum desired image resolution
     max_resolution = (9999, 9999)       # Maximum desired image resolution
-    max_missed = 20                     # Max number of failed images before exit
+    max_missed = 10                     # Max number of failed images before exit
     number_of_workers = 1               # Number of "workers" used
     keep_filenames = False              # Keep original URL image filenames
 
